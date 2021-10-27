fadd08b7487d12dc28024f1371e6c7e9347c3778
Gerard Lakin <gerardlakin@gmail.com>
2021-10-26 23:58:28 -0500

    Added ngram plots to data_exploration.R. Wrote data exploration report.

--- data_exploration.R
+++ data_exploration.R
@@ -60,9 +60,10 @@ news_sample <- news[1:20000]
 
 library(tidytext)
 library(dplyr)
-library(Rcpp)
+#library(Rcpp)
 library(stopwords)
-library(stringr)
+library(tidyr)
+#library(stringr)
 
 #custom_stopwords = (tibble(word = c("iâ", "itâ", "ã", "â", "å")))
 
@@ -122,19 +123,28 @@ head(news_bigramcounts)
 #Trigrams
 twitter_trigramcounts <- twitter_df %>% 
   unnest_tokens(trigram, Text, token = "ngrams", n=3) %>%
-  count(trigram, sort = TRUE)
+  count(trigram, sort = TRUE) 
+  
 blog_trigramcounts <- blog_df %>%
   unnest_tokens(trigram, Text, token = "ngrams", n=3) %>%
   count(trigram, sort = TRUE)
 news_trigramcounts <- news_df %>%
   unnest_tokens(trigram, Text, token = "ngrams", n=3) %>%
-  count(trigram, sort = TRUE)
+  drop_na() %>%
+  count(trigram, sort = TRUE) 
+
+twitter_trigramcounts
+
 
 head(twitter_trigramcounts)
 head(blog_trigramcounts)
 head(news_trigramcounts)
 
 library(ggplot2)
+
+###########################
+# WORDCOUNT PLOTS
+##########################
 
 twitter_wordcount_plot <- ggplot(data = head(twitter_wordcounts, n=10), aes(x = reorder(word, -n), y = n)) +
   #coord_cartesian(ylim = c(y_min, y_max)) +
@@ -186,4 +196,135 @@ news_wordcount_plot <- ggplot(data = head(news_wordcounts, n=10), aes(x = reorde
 news_wordcount_plot
 
 
+###########################
+# BIGRAM PLOTS
+###########################
 
+twitter_bigram_plot <- ggplot(data = head(twitter_bigramcounts, n=10), aes(x = reorder(bigram, -n), y = n)) +
+  #coord_cartesian(ylim = c(y_min, y_max)) +
+  geom_bar(stat="identity", color = "black", fill = "slategray2") +
+  labs(title = "Most Common Bigrams on Twitter (20k Tweet Sample)", x = "Word", y = "Count") +
+  theme(plot.title = element_text(size=14, face="bold",
+                                  margin = margin(10, 0, 10, 0)),
+        axis.title = element_text(size=11),
+        axis.text.x = element_text(size=11, vjust=0.5),
+        axis.text.y = element_text(size=11, vjust=0.5),
+        axis.ticks.x = element_blank()) +
+  #scale_y_continuous(labels = scales::number_format(accuracy = 0.001)) +
+  geom_text(aes(label = n, y = n + 20),
+            position = position_dodge(0.9), size=4)
+
+twitter_bigram_plot
+
+blog_bigram_plot <- ggplot(data = head(blog_bigramcounts, n=10), aes(x = reorder(bigram, -n), y = n)) +
+  #coord_cartesian(ylim = c(y_min, y_max)) +
+  geom_bar(stat="identity", color = "black", fill = "slategray2") +
+  labs(title = "Most Common Words in Blogs (20k Blog Sample)", x = "Bigram", y = "Count") +
+  theme(plot.title = element_text(size=14, face="bold",
+                                  margin = margin(10, 0, 10, 0)),
+        axis.title = element_text(size=11),
+        axis.text.x = element_text(size=11, vjust=0.5),
+        axis.text.y = element_text(size=11, vjust=0.5),
+        axis.ticks.x = element_blank()) +
+  #scale_y_continuous(labels = scales::number_format(accuracy = 0.001)) +
+  geom_text(aes(label = n, y = n + 4200),
+            position = position_dodge(0.9), size=4)
+
+blog_bigram_plot
+
+
+news_bigram_plot <- ggplot(data = head(news_bigramcounts, n=10), aes(x = reorder(bigram, -n), y = n)) +
+  #coord_cartesian(ylim = c(y_min, y_max)) +
+  geom_bar(stat="identity", color = "black", fill = "slategray2") +
+  labs(title = "Most Common Words in News (20k Article Sample)", x = "Bigram", y = "Count") +
+  theme(plot.title = element_text(size=14, face="bold",
+                                  margin = margin(10, 0, 10, 0)),
+        axis.title = element_text(size=11),
+        axis.text.x = element_text(size=11, vjust=0.5),
+        axis.text.y = element_text(size=11, vjust=0.5),
+        axis.ticks.x = element_blank()) +
+  #scale_y_continuous(labels = scales::number_format(accuracy = 0.001)) +
+  geom_text(aes(label = n, y = n + 380),
+            position = position_dodge(0.9), size=4)
+
+news_bigram_plot
+
+
+###########################
+# TRIGRAM PLOTS
+###########################
+
+twitter_trigram_plot <- ggplot(data = head(twitter_trigramcounts, n=10), aes(x = reorder(trigram, -n), y = n)) +
+  #coord_cartesian(ylim = c(y_min, y_max)) +
+  geom_bar(stat="identity", color = "black", fill = "slategray2") +
+  labs(title = "Most Common Bigrams on Twitter (20k Tweet Sample)", x = "Word", y = "Count") +
+  theme(plot.title = element_text(size=14, face="bold",
+                                  margin = margin(10, 0, 10, 0)),
+        axis.title = element_text(size=11),
+        axis.text.x = element_text(size=11, vjust=0.5),
+        axis.text.y = element_text(size=11, vjust=0.5),
+        axis.ticks.x = element_blank()) +
+  #scale_y_continuous(labels = scales::number_format(accuracy = 0.001)) +
+  geom_text(aes(label = n, y = n + 20),
+            position = position_dodge(0.9), size=4)
+
+twitter_trigram_plot
+
+blog_trigram_plot <- ggplot(data = head(blog_trigramcounts, n=10), aes(x = reorder(trigram, -n), y = n)) +
+  #coord_cartesian(ylim = c(y_min, y_max)) +
+  geom_bar(stat="identity", color = "black", fill = "slategray2") +
+  labs(title = "Most Common Words in Blogs (20k Blog Sample)", x = "Bigram", y = "Count") +
+  theme(plot.title = element_text(size=14, face="bold",
+                                  margin = margin(10, 0, 10, 0)),
+        axis.title = element_text(size=11),
+        axis.text.x = element_text(size=11, vjust=0.5),
+        axis.text.y = element_text(size=11, vjust=0.5),
+        axis.ticks.x = element_blank()) +
+  #scale_y_continuous(labels = scales::number_format(accuracy = 0.001)) +
+  geom_text(aes(label = n, y = n + 4200),
+            position = position_dodge(0.9), size=4)
+
+blog_trigram_plot
+
+
+news_trigram_plot <- ggplot(data = head(news_trigramcounts, n=10), aes(x = reorder(trigram, -n), y = n)) +
+  #coord_cartesian(ylim = c(y_min, y_max)) +
+  geom_bar(stat="identity", color = "black", fill = "slategray2") +
+  labs(title = "Most Common Words in News (20k Article Sample)", x = "Trigram", y = "Count") +
+  theme(plot.title = element_text(size=14, face="bold",
+                                  margin = margin(10, 0, 10, 0)),
+        axis.title = element_text(size=11),
+        axis.text.x = element_text(size=11, vjust=0.5),
+        axis.text.y = element_text(size=11, vjust=0.5),
+        axis.ticks.x = element_blank()) +
+  #scale_y_continuous(labels = scales::number_format(accuracy = 0.001)) +
+  geom_text(aes(label = n, y = n + 35),
+            position = position_dodge(0.9), size=4)
+
+news_trigram_plot
+
+####################
+# DICTIONARY SIZE
+####################
+
+blog_word_total <- sum(blog_wordcounts$n)
+
+for (i in 1:length(blog_wordcounts$word)) {
+  if (sum(blog_wordcounts[1:i,2]) >= (blog_word_total*0.5)) {
+    i_50 <- i
+    break
+  }
+}
+
+
+for (i in 1:length(blog_wordcounts$word)) {
+  if (sum(blog_wordcounts[1:i,2]) >= (blog_word_total*0.9)) {
+    i_90 <- i
+    break
+  }
+}
+
+i_50
+blog_wordcounts[i_50,1]
+i_90
+blog_wordcounts[i_90,1]
